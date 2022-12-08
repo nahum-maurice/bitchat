c11156984a8dd420c070f266e71784e52e6df353
Nahum Maurice <nahum.maurice.nm@gmail.com>
2022-12-08 19:45:54 +0300

    Added Message data structures

---
+++ tests/book/test_message.py
@@ -0,0 +1,35 @@
+from bitchat.book.message import Message, MessageHeader
+
+receiver = "0x9238494ffac739347af9de9359abfd24"
+sender = "0x9238494ffa8929347af9de9352957d24"
+
+
+def header_correct():
+    header = MessageHeader(
+        receiver=receiver,
+        sender=sender,
+        version="0.1.0",
+        timestamp="1670501700620"
+    )
+    return header
+
+
+def message_correct():
+    header = MessageHeader(
+        receiver=receiver,
+        sender=sender,
+        version="0.1.0",
+        timestamp="1670501700620"
+    )
+    data = "0x939429jfds9bigyfksadfvo29fisdvibsf02fi"
+
+    msg = Message(header=header, data=data)
+    return msg
+
+
+def test():
+    header = header_correct()
+    msg = message_correct()
+
+    assert (isinstance(header, MessageHeader))
+    assert (isinstance(msg, Message))
