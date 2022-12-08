cabf46fd8a20bef82ee88070d703c69198da4d01
Nahum Maurice <nahum.maurice.nm@gmail.com>
2022-12-08 21:42:54 +0300

    Added support for __repr__ and adding messages in Book

--- bitchat/book/book.py
+++ bitchat/book/book.py
@@ -4,8 +4,10 @@ Written by Nahum Maurice on Thur, December 8, 2022
 This is the data structure that contains all messages. It's public, and is
 shared by all the nodes in the network.
 """
-from abc import ABCMeta, abstractmethod
-from typing import Type
+from __future__ import annotations
+
+from abc import ABCMeta, abstractmethod, abstractproperty
+from typing import Optional
 
 from ..errors.singleBookInstanceViolation import SingleBookInstanceViolation
 from .message import Message
@@ -13,9 +15,24 @@ from .message import Message
 
 class BookInterface(metaclass=ABCMeta):
 
-    @abstractmethod
     def __init__(self) -> None:
-        ...
+        """"""
+
+    @abstractmethod
+    def instance(self) -> Optional[BookInterface]:
+        """"""
+
+    @abstractproperty
+    def messages(self) -> list[Message]:
+        """"""
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        """"""
+
+    @abstractmethod
+    def add_message(self, message: Message) -> None:
+        """"""
 
 
 class Book(BookInterface):
@@ -26,7 +43,7 @@ class Book(BookInterface):
     # The algorithm version
     __version = 0
 
-    def __init__(self) -> None:
+    def __init__(self, first_node: str) -> None:
         # There should always be one instance of the book and this instance
         # should be the one shared by every node, but only be modifiable by
         # the master node. Therefore, this class follows the pattern of
@@ -46,11 +63,33 @@ class Book(BookInterface):
 
         # Each book is initiated by the first node connecting to the network.
         # This should be the address of the node.
-        self.__firstNode: str = ""
+        self.__first_node: str = first_node
 
         # Each book contains a list of messages, these are encrypted message
         # with addresses
-        self.__messages: list[Type[Message]] = []
+        self.__messages: list[Message] = []
 
         # Assigning __instance to the newly created one
         Book.__instance = self
+
+    def __repr__(self) -> str:
+        messages = ""
+        for message in self.__messages:
+            messages += f"\nIndex --> {self.__messages.index(message)} | " + \
+                f"Timestamp --> {message.header.timestamp} | receiver " + \
+                f"--> {message.header.receiver}"
+
+        return f"Book(ID: {self.__id}, First Node: {self.__first_node} " + \
+               f"Lenght: {len(self.__messages)})" + \
+               f"\nMessages: {messages}"
+
+    def add_message(self, message: Message) -> None:
+        self.__messages.append(message)
+
+    @staticmethod
+    def instance() -> Optional[Book]:
+        return Book.__instance
+
+    @property
+    def messages(self) -> list[Message]:
+        return self.__messages
--- tests/book/test_book.py
+++ tests/book/test_book.py
@@ -1,17 +1,51 @@
+from typing import Optional
+
 import pytest
 
 from bitchat.book.book import Book, SingleBookInstanceViolation
+from bitchat.book.message import Message, MessageHeader
 
 
-def book_correct() -> None:
-    # The single instance of the Book
-    s = Book()
-    t = Book()
-    print(s)
-    print(t)
+def one_instance() -> None:
+    first_node = "0x9238494ffac739347af9de9359abfd24"
+    # The single authorized instance of the Book
+    book = Book(first_node=first_node)
+
+    representation = book.__repr__()
+    assert (type(representation) == str)
+
+
+def second_instance() -> None:
+    first_node = "0x9238494ffac739347af9de9359abfd24"
+    # Attemps to create a second Book
+    Book(first_node=first_node)
+
+
+def adding_message() -> None:
+    receiver = "0x9238494ffac739347af9de9359abfd24"
+    sender = "0x9238494ffa8929347af9de9352957d24"
+
+    msg_header: MessageHeader = MessageHeader(
+        receiver=receiver,
+        sender=sender,
+        version="0.1.0",
+        timestamp="1670501700620"
+    )
+    data = "0x939429jfds9bigyfksadfvo29fisdvibsf02fi"
+    msg = Message(header=msg_header, data=data)
+
+    # Getting the single instance
+    book: Optional[Book] = Book.instance()
+    if (book is not None):
+        book.add_message(msg)
+        assert (len(book.messages) == 1)
 
 
 def test_book() -> None:
+    one_instance()
+
     with pytest.raises(SingleBookInstanceViolation):
         # Should not allow multiple instances
-        book_correct()
+        second_instance()
+
+    adding_message()
